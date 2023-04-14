8655c7aaf8db0926ddc92f00798037e84e92300a
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-04-14 09:20:37 +0000

    updated 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,6 @@
 #!/bin/usr/bin/node
 
 //A script that print two Argument passed to it in the following format: "is"
-
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
+args1 = process.argv[2];
+args2 = process.argv[3];
+console.log(args1 + ' ' + is + ' ' + args2);
