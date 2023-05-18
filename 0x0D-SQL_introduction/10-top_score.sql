6e98372d4094e3ec23367cd3a411da44574aa590
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-05-18 00:16:19 +0000

    An order score of second table

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists all records of table second table
+-- hbtn_0c_0 in MySQL server
+-- results display score and name
+-- ordered by score
+SELECT score, name FROM second_table ORDER BY score DESC, name DESC;
