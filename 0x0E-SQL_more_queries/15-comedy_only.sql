474e39f7ae3187b66a8df855a6fa4045ba7c0e93
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-05-18 03:45:51 +0000

    all file been commited

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,6 +2,7 @@
 -- 'tv_genres' table contains only one record where name = Comedy
 -- Results must be sorted in ascending order by the show title
 -- You can only use one SELECT statement
+
 SELECT tv_shows.title
 FROM tv_shows
 INNER JOIN tv_show_genres m ON tv_shows.id = m.show_id
