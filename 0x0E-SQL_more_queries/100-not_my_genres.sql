f00cac125afa0f5cccbec887ac284f45299891c7
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-05-18 03:03:31 +0000

    uses the hbtn_0d_tvshows database

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,15 @@
+-- Use 'hbtn_0d_tvshows' to list all genres not linked to show 'Dexter'
+-- 'tv_shows' table contains only one record where title = Dexter
+-- Each record should display tv_genres.name
+-- Results must be sorted ascending order by genre name
+-- You can use max of two SELECT statements
+SELECT gtv.name
+FROM tv_genres g
+WHERE gtv.name NOT IN (
+      SELECT gtv.name
+      FROM tv_genres gtv
+      INNER JOIN tv_show_genres mo ON gtv.id = mo.genre_id
+      INNER JOIN tv_shows so ON mo.show_id = so.id
+      WHERE so.title = 'Dexter'
+)
+ORDER BY gtv.name ASC;
