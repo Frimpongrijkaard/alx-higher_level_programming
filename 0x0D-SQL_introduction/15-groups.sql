1e65d93f05aac97f046811de06adb586b71c156e
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-05-18 15:38:23 +0000

    file format change

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,5 @@
--- lists the number of records with same score
---in the second table of the hbtn_0c_0
--- in MySQL server
-SELECT score, COUNT(score) AS number 
-FROM second_table
-GROUP BY score
-ORDER BY score DESC;
+-- Lists the number of records with the same score
+-- in the table `second_table` of the database `hbtn_0c_0`
+-- in MySQL Server.
+
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY score DESC;
