9c2cb04cb16dde6e78ae3470f4ae2266f8e674ec
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-06-14 18:16:18 +0000

    one file modified

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/bin/usr/bin/node
 
 // A script that print two Argument passed to it in the following format: "is"
-args1 = process.argv[2]
-args2 = process.argv[3]
-console.log(args1 + ' ' + is + ' ' + args2)
+args1 = process.argv[2];
+args2 = process.argv[3];
+console.log(args1 + ' ' + is + ' ' + args2);
