b2c129ee38f62a22b09328c684371d644ee6bd4c
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-06-14 17:16:56 +0000

    one file modified

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,5 +3,5 @@
 // Script that print the first argument passed to it
 
 const args = process.argv.slice(2);
-const oneArgument = args[0] === 'undefined' ? 'No argument' : process.argv[1];
+const oneArgument = args[0] === 'undefined' ? 'No argument' : args[1];
 console.log(oneArgument);
