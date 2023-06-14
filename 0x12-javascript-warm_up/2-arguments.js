ea6223e190b290c6a5ed5d711053f459b6b5e89e
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-06-14 13:59:01 +0000

    added modified file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,12 +3,9 @@
 //script that print a message depending of the number of arguments passed
 
 const args = process.argv.slice(2);
-if (args.length === 0) {
-	console.log("NO argument");
-}
-else if (args.length === 1) {
-	console.log("Argument found");
-}
-else {
-	console.log("Argument found");
+
+if (args.length === 0) { console.log('NO argument')
+} else if (args.length === 1) { console.log('Argument found')
+} else {
+    console.log('Argument found')
 }
