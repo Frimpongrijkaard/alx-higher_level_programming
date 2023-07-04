dd68ababf31684a9a1dd71c605ac30bcb4aa1b18
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-04 10:14:18 +0000

    solution to python #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import urllib.request
 """define a function that fetch url content
 Usage: ./0-bntn_status.py
 
@@ -9,19 +8,15 @@ Usage: ./0-bntn_status.py
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    fetch_url(url)
+    url = "https://alx-intranet.hbtn.io/status"""
 
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
 
-    def fetch_url(url):
-        """function that fetches url content"""
-        request = urllib.request.Request(url)
-        with urllib.request.urlopen(request) as response:
-            content = response.read()
-            utf8_content = content.decode('utf-8')
-
-        print("Body response:")
-        print("\t- type: {} ".format(type(content)))
-        print("\t- content: {} ".format(content))
-        print("\t- utf8 content: {}".format(utf8_content))
+    print("Body response:")
+    print("\t- type: {} ".format(type(content)))
+    print("\t- content: {} ".format(content))
+    print("\t- utf8 content: {}".format(utf8_content))
 
