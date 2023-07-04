857e6cbc9fd68018970294db1a6dba3d7667e437
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-04 04:00:09 -0700

    siolution to python network #1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
+"""define a script that take url, send and display X_id"""
+
 import urllib.request
 import sys
 
-"""define a script that take url, send and display X_id"""
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-def X_request_id(url):
-    """function that display the x_request_id"""
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         content_id = response.headers.get('X-Request-Id')
-    print(content_id)
-
-if __name__ == "__main__":
-    url = sys.argv[1]
-    X_request_id(url)
+        print(content)
