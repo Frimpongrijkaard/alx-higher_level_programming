f64da3cad2f799066e7eb22d6705a8888b5d7b9e
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-04 06:34:11 -0700

    authorized file

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,23 +4,9 @@ and uses the GitHub API to display your id
 """
 import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-
-    headers = {"Authentication": f"Token {access_token}"}
-    url = "https://api.github.com/user"
-    response = requests.get(url, headers=headers)
-    code = response.status_code
-
-    if code == 200:
-        json_d = response.json()
-        j_id = json_d.get('id')
-        if j_id:
-            print(j_id)
-        else:
-            print("None")
-    else:
-        print("None")
-
+    authorized = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    response = requests.get("https://api.github.com/user", auth=authorized)
+    print(response.json().get("id"))
