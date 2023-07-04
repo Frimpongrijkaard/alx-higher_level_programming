a6af882c77b15d179d46c5296d2dd4407278c658
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-04 13:08:32 +0000

    solution to python #1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
+"""python script that take letter and send post to url
+Usage: ./8-json_api.py <letter>
+
+"""
 import requests
 import sys
 
-def  Search_API(q):
-    """function that take letter and send post to url"""
+if __name__ == "__main__":
+    q = "" if len(sys.argv) == 1 else sys.argv[1]
+
 
     url = "http://0.0.0.0:5000/search_user"
     data = {'q' : q}
 
     response = request.post(url, data=data)
-    json_d = response.json()
-    if isinstance(json_d, dict):
-        if 'id' in json_d and 'name' in json_d:
-            print("[{}] {}".format(json_d['id'], json_d['name']))
-        else:
-            print("No result")
-    else:
+    try:
+        json_d = response.json()
+        if isinstance(json_d, dict):
+            if 'id' in json_d and 'name' in json_d:
+                print("[{}] {}".format(json_d['id'], json_d['name']))
+            else:
+                print("No result")
+    except ValueError:
         print("Not a valid JSON")
 
-if __name__ == "__main__":
-    q = sys.argv[1]
-    if len(q) < 2:
-        q = " "
-    else:
-        q = sys.argv[1]
-    search_API(q)
+
