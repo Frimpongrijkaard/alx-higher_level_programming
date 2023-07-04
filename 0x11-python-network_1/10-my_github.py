bc0f89780acf94cd65df1673cd060cad1cfd99dc
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-04 07:19:40 -0700

    solution to python network #1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Python script that takes your GitHub credentials (username and password) 
-and uses the GitHub API to display your id
-"""
+"""Python script that takes credential from GitHub API to display your id"""
+
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
