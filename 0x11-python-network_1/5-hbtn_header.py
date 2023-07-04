21db82d107868bbaabcd8b249624ac344d369e1f
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-04 06:11:40 -0700

    solution to python script #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
+ """python script that takes in url ans return the x-Request-id"""
 
 import sys
 import requests
 
-def response_header_value(url):
-    """function that takes in url ans return the x-Request-id"""
-    response = requests.get(url)
-    value = response.headers.get("X-Request-Id")
-    print(value)
-
 if __name__ == "__main__":
     url = sys.argv[1]
-    response_header_value(url)
+
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
