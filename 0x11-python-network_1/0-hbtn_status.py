5ede24e88a001ce33ef1f46e20a535580a074dc5
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-04 03:31:38 +0000

    solution to python #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,19 +3,21 @@
 import urllib.request
 """define a function that fetch url content"""
 
-def fetch_url(url):
-    """function that fetches url content"""
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        content = response.read()
-        utf8_content = content.decode('utf-8')
-
-    print("Body response:")
-    print("\t- type: {} ".format(type(content)))
-    print("\t- content: {} ".format(content))
-    print("\t- utf8 content: {}".format(utf8_content))
-
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     fetch_url(url)
+
+
+    def fetch_url(url):
+        """function that fetches url content"""
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as response:
+            content = response.read()
+            utf8_content = content.decode('utf-8')
+
+        print("Body response:")
+        print("\t- type: {} ".format(type(content)))
+        print("\t- content: {} ".format(content))
+        print("\t- utf8 content: {}".format(utf8_content))
+
