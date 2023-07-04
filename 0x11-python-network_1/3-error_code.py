74426434bc0589d5b91c21274a7e86d30908c51d
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-04 05:02:16 -0700

    siolution to python network #1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""function that display Error code from url"""
 import sys
 import urllib.request
 from urllib.error import HTTPError
 
-def Error_code(url):
-    """function that display Error code from url"""
-    try:
-        request = urllib.request.Request(url)
-        with urllib.request.urlopen(request) as respomse:
-            body = response.read().encode('utf-8')
-    execpt HTTPError as e:
-        print("Error code: {}".format(e.code))
-
 if __name__ == "__main__":
     url = sys.argv[1]
-    Error_code(url)
+
+    try:
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
+
