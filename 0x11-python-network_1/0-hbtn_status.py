5a2d053dd36d6e3a8bc0d87748146e8638a3b618
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-04 03:50:17 -0700

    siolution to python network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,14 +4,13 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"""
-
+    url = "https://alx-intranet.hbtn.io/status"
 
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
         content = response.read()
         utf8_content = content.decode('utf-8')
         print("Body response:")
-        print("\t- type: {} ".format(type(content)))
-        print("\t- content: {} ".format(content))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(utf8_cont))
