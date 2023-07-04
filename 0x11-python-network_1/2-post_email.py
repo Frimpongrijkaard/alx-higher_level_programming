1677d63ac6f0c8f6f824b439c4300d67ee145e2a
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-04 04:27:14 -0700

    solution of the python network #1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""define post request function the response to email"""
+"""define post request script that response to given email
+Usage: ./2-post_email.py <URL> <Email>
+    - Display the body of the response
+"""
 import urllib.request
 import urllib.parse
 import sys
@@ -9,7 +12,7 @@ if __name__ == "__main__":
     email = sys.argv[2]
     
     data = urllib.parse.urlencode({'email' : email}).encode('utf-8')
-    request = urllib.request.Request(url, data=data, method='POST')
+    request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
         body = response.read().decode('utf-8')
         print(body)
