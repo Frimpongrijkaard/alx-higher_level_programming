fd7316322a92f6338fca401094ec332b4af96008
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-07-04 04:32:03 -0700

    solution of the python network #1

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,9 +9,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
+    mail = {"email" :sys.argv[2]}
     
-    data = urllib.parse.urlencode({'email' : email}).encode('utf-8')
+    data = urllib.parse.urlencode(mail).encode('ascii')
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
         body = response.read().decode('utf-8')
